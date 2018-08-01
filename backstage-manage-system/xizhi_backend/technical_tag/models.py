061056e0f6dea37bb49401f4c03427f5a90604b1
SunShineToMiaoMiao <1174975432@qq.com>
2018-08-01 23:09:15 +0800

    生成表

--- backstage-manage-system/xizhi_backend/technical_tag/models.py
+++ backstage-manage-system/xizhi_backend/technical_tag/models.py
@@ -1,15 +1,1 @@
-from django.db import models
-
-
-class TechnicalTag(models.Model):
-    id = models.UUIDField(primary_key=True, editable=False)
-    name = models.CharField(max_length=50)
-    svg_class = models.CharField(max_length=50)
-    is_enable = models.BooleanField()
-    create_user = models.CharField(max_length=250)
-    # 在model对象第一次被创建时,将字段的值设置为创建时的时间,
-    # 以后修改对象时,字段的值不会再更新。
-    create_time = models.DateTimeField(auto_now_add=True)
-    update_user = models.CharField(max_length=250)
-    # 在保存该字段时,将其值设置为当前时间,并且每次修改model,都会自动更新
-    update_time = models.DateTimeField(auto_now=True)
+from django.db import modelsimport uuidclass TechnicalTag(models.Model):    # 纯随机数，与机器无关, 相重的几率很小。通常生成用户id用这个。    id = models.UUIDField(primary_key=True, auto_created=True, default=uuid.uuid4(), editable=False)    name = models.CharField(max_length=50)    svg_class = models.CharField(max_length=50)    is_enabled = models.BooleanField(default=True)    is_delete = models.BooleanField(default=False)    create_user = models.CharField(max_length=250)    # 在model对象第一次被创建时,将字段的值设置为创建时的时间,    # 以后修改对象时,字段的值不会再更新。    create_time = models.DateTimeField(auto_now_add=True)    update_user = models.CharField(max_length=250)    # 在保存该字段时,将其值设置为当前时间,并且每次修改model,都会自动更新    update_time = models.DateTimeField(auto_now=True)
