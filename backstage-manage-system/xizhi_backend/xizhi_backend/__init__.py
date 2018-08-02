a213ed824128cd48505e0bd81746a2b39a783601
SunShineToMiaoMiao <1174975432@qq.com>
2018-08-02 19:14:33 +0800

    忽视数据库编译

--- backstage-manage-system/xizhi_backend/xizhi_backend/__init__.py
+++ backstage-manage-system/xizhi_backend/xizhi_backend/__init__.py
@@ -1,11 +1,1 @@
 # from .models import *
-let code = ''
-if(localStorage.AppType === 1){ // 分销
-code = '02'
-}else if(localStorage.AppType === 2){ //代理
-code = '01'
-}
-
-    let param = {
-    platFormCode:code
-}
